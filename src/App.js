cfa1a20075f198f482df9db37abed77b0b32022f
Aryansh <aryanshg.ug21.cs@nitp.ac.in>
2023-09-19 23:08:20 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import {data} from './data.js';
 import './App.css';
 
 function App() {
+  // console.log(data);
+
+  const [search,setSearch]=useState('')
+  // console.log(search)
+
+  const styles = {
+    backgroundColor: 'white',
+    width: '500px',
+    marginBottom: '10px',
+    padding: '10px',
+    color: 'green',
+    boxShadow: 'rgb(0,0,0,0.44) 0px 5px 5px',
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App'>
+        <div>
+        <h1>Sample Task-1</h1>
+
+      <input type='text' 
+      onChange={(e)=> setSearch(e.target.value)} 
+      placeholder='Search for the Gadgets'></input>
+      <button>Search</button>
+        </div>
+  
+      <div className='Container'>
+      {data.filter((ob)=>{
+        if(search.toLowerCase()=='')
+        return ob;
+        else if(ob.title.toLowerCase().includes(search.toLowerCase()))
+        return ob;
+      }).map(
+          (ob)=> {
+            return (
+              <div key={ob.id} style={styles}>
+          <h2>Category: {ob.category}</h2>
+          <img src={ob.thumbnail}></img>
+          <h3>{ob.title}</h3>
+          <h4>{ob.description}</h4>
+          <h4>Price: {ob.price} $</h4>
+          <h4>%Discount: {ob.discountPercentage}</h4>
+          <h4>Rating:{ob.rating}</h4>
+          <h4>Stock:{ob.stock}</h4>
+          <h4>Brand:{ob.brand}</h4>
+        </div>)
+        }
+        )}
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
